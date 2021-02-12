0d3cd1ca40820f58725c846ec13e4186bb8ba530
Giacomo <v.giacomo3@gmail.com>
2021-02-12 08:10:28 +0100

    first commit

---
+++ Notes.WebApi/Notes.WebApi/Controllers/NotesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Notes.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotesController : ControllerBase
+    {
+
+        private readonly ILogger<NotesController> _logger;
+
+        public NotesController(ILogger<NotesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("My first note");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# React---ASP.NET-Core-3.1-Application-Notes
